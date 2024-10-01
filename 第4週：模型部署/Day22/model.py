f63e85d6ef3ff329b7b6807660f0379aa3ef90d4
HuangZM-0825 <cgiksjf987@gmail.com>
2024-10-02 00:45:15 +0800

    Optimize Dockerfile and update Chinese translations for model deployment tasks

---
+++ 第4週：模型部署/Day22/model.py
@@ -0,0 +1,39 @@
+# 透過 CNN 模型辨識 MNIST 手寫數字資料集
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+
+# 下載並預處理 MNIST 資料集
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape((x_train.shape[0], 28, 28, 1)) / 255.0
+x_test = x_test.reshape((x_test.shape[0], 28, 28, 1)) / 255.0
+
+# 建立 CNN 模型
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(10, activation='softmax')
+])
+
+# 編譯模型
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 訓練模型
+model.fit(x_train, y_train, epochs=10, batch_size=64)
+
+# 保存模型
+model.save('mnist_cnn_model.h5')
+
+# 評估模型
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print(f'Test accuracy: {test_acc}')
